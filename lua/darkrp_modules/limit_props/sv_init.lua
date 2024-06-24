26288ccf2389ddf8fb7039728531213617c76df5
Haodong Mo <mohaodong@gmail.com>
2024-06-24 21:15:27 +1000

    Amogus

--- lua/darkrp_modules/limit_props/sv_init.lua
+++ lua/darkrp_modules/limit_props/sv_init.lua
@@ -1,8 +1,27 @@
-local maxs = 512
+local maxs = 300
 local maxvol = 256 * 256 * 256
 
+GAMEMODE.PropLimitTable = {}
+
 hook.Add("PlayerSpawnedProp", "ARCRP_LimitProps", function(ply, model, ent)
+    if GAMEMODE.PropLimitTable[model] != nil then
+        if GAMEMODE.PropLimitTable[model] == false then
+            SafeRemoveEntity(ent)
+        end
+
+        return
+    end
+
     if !IsValid(ent) then return end
+
+    local physobj = ent:GetPhysicsObject()
+
+    if !IsValid(physobj) then
+        SafeRemoveEntity(ent)
+        DarkRP.notify(ply, 1, 3, "Prop is not valid!")
+        GAMEMODE.PropLimitTable[model] = false
+        return
+    end
 
     local min, max = ent:WorldSpaceAABB()
 
@@ -13,12 +32,16 @@ hook.Add("PlayerSpawnedProp", "ARCRP_LimitProps", function(ply, model, ent)
     if long > maxs or wide > maxs or tall > maxs then
         SafeRemoveEntity(ent)
         DarkRP.notify(ply, 1, 3, "Prop is too big!")
+        GAMEMODE.PropLimitTable[model] = false
         return
     end
 
     if long * wide * tall > maxvol then
         SafeRemoveEntity(ent)
         DarkRP.notify(ply, 1, 3, "Prop is too big!")
+        GAMEMODE.PropLimitTable[model] = false
         return
     end
+
+    GAMEMODE.PropLimitTable[model] = true
 end)

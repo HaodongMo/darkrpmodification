357dcd191756222ec68f4ade5f36658cd38a0f33
Haodong Mo <mohaodong@gmail.com>
2024-06-24 17:13:46 +1000

    Prop limit, disable stuff

---
+++ lua/darkrp_modules/limit_props/sv_init.lua
@@ -0,0 +1,24 @@
+local maxs = 1024
+local maxvol = 512 * 512 * 512
+
+hook.Add("PlayerSpawnedProp", "ARCRP_LimitProps", function(ply, model, ent)
+    if !IsValid(ent) then return end
+
+    local min, max = ent:WorldSpaceAABB()
+
+    local long = max[1] - min[1]
+    local wide = max[2] - min[2]
+    local tall = max[3] - min[3]
+
+    if long > maxs or wide > maxs or tall > maxs then
+        SafeRemoveEntity(ent)
+        DarkRP.notify(ply, 1, 3, "Prop is too big!")
+        return
+    end
+
+    if long * wide * tall > maxvol then
+        SafeRemoveEntity(ent)
+        DarkRP.notify(ply, 1, 3, "Prop is too big!")
+        return
+    end
+end)
